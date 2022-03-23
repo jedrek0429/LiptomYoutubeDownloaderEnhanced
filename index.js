0feb12e00009bfbdf542359fa65628c7e7f34a4a
Jędrek <a.bansleben0429@gmail.com>
2022-03-23 21:25:49 +0100

    what is wrong

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8000;
 app.get('/', (req, res) => {
     if (req.query.format && req.query.url) {
         if (!["mp3", "mp4"].includes(req.query.format)) return res.sendStatus(400);
-        return res.redirect(`https://adoredvictoriousengineers.liptom3281.repl.co/${req.query.format}?URL=${req.query.url}`)
+        return res.redirect(`https://adoredvictoriousengineers.liptom3281.repl.co/${req.query.format}?URL=${req.query.url}`);
     }
     res.sendFile(path.join(__dirname + "/index.html"));
 });
